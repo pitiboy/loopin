62aa14203ec2036add5b200c0b07ee3dabd52af4
pitiboy <peter.horvath@finnoconsult.at>
2019-12-12 23:57:58 +0100

    add wait for piano key loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import MIDISounds from 'midi-sounds-react';
 // import Looper from 'react-looper';
 import Looper, { PlayTypes } from './Looper';
@@ -10,16 +10,28 @@ import PianoKeyboard from './Layout/PianoKeyboard';
 
 
 const App: React.FC = () => {
-  const midiKeys = useRef<MIDISoundsType>(null);
   const midiDrums = useRef<MIDISoundsType>(null);
   const midiBass = useRef<MIDISoundsType>(null);
+  const midiKeys = useRef<MIDISoundsType>(null);
+  const [bassChord, setBassChord] = useState(376); // good bass: 372, 376
+  const [pianoChord, setPianoChord] = useState(847);
+
   // TODO: strings Keys, , Strings
   const playChord = (pitches: number | number[]) => {
-    if (midiDrums !== null && midiDrums.current !== null) {
+    if (midiKeys !== null && midiKeys.current !== null) {
       const defaultPitches: number[] = [];
       const playPitches = defaultPitches.concat(pitches);
-      midiDrums.current.playChordNow(847, playPitches, 1);
+      midiKeys.current.playChordNow(pianoChord, playPitches, 0.5);
     }
+  }
+
+  const cacheInstrument = (instrument: number) => {
+    console.log('trying to cache instrument', instrument);
+    if (midiKeys.current) {
+      midiKeys.current.cacheInstrument(instrument);
+      midiKeys.current.player.loader.waitLoad(() => playChord(50));
+    }
+    setPianoChord(instrument);
   }
   const myBPM = 80;
   return (
@@ -34,17 +46,19 @@ const App: React.FC = () => {
         <Looper playType={PlayTypes.even} bpm={myBPM * 4} source={() => midiDrums.current && midiDrums.current.playDrumsNow([39])} render={SquareLooperRenderer} />
 
         <Section title="Bass">
-          <MIDISounds ref={midiBass} appElementName="root" instruments={[376]} />
+          <MIDISounds ref={midiBass} appElementName="root" instruments={[bassChord]} />
+           <input type="number" value={bassChord} onChange={e => setBassChord(parseInt(e.target.value, 10))} min={366} max={446} />
         </Section>
-        <Looper playType={PlayTypes.odd} bpm={myBPM * 4} source={() => midiBass.current && midiBass.current.playChordNow(376, [30], 1)} render={SquareLooperRenderer} />
+        <Looper playType={PlayTypes.odd} bpm={myBPM * 4} source={() => midiBass.current && midiBass.current.playChordNow(bassChord, [30], 1)} render={SquareLooperRenderer} />
       </Metronome>
 
 
       <Section title="Keys">
         <MIDISounds ref={midiKeys} appElementName="root" instruments={[1,2,3,305, 847]} />
+        <input type="number" value={pianoChord} onChange={e => cacheInstrument(parseInt(e.target.value, 10))}  min={0} max={1393} />
       </Section>
 
-      <PianoKeyboard playNote={(note) => playChord(note)} />
+      <PianoKeyboard playNote={(note) => playChord(note)} />
     </div>
   );
 }
--- src/react-midi-sounds.d.ts
+++ src/react-midi-sounds.d.ts
@@ -3,4 +3,10 @@ declare module 'midi-sounds-react';
 interface MIDISoundsType {
   playChordNow: (instrument: number, pitches: number[], duration: number) => void;
   playDrumsNow: (drums: [number]) => void;
+  cacheInstrument: (instrument: number) => void;
+  player: {
+    loader: {
+      waitLoad: (func: () => void) => void;
+    }
+  }
 }
