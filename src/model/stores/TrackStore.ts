de038af168e1a4396a89cf329bdb39427348f4f8
pitiboy <peter.horvath@finnoconsult.at>
2019-12-25 16:50:02 +0100

    rename types.tsx

--- src/model/stores/TrackStore.ts
+++ src/model/stores/TrackStore.ts
@@ -36,27 +36,29 @@ interface TrackProps extends TackSoundProps, TackControlProps {
 // https://stackoverflow.com/questions/52641907/how-to-get-mobx-decorators-to-work-with-create-react-app-v2
 export default class TrackStore {
   // constructor() {
-    // }
+  // }
 
 
-  @observable tracks:TrackProps[] = [{
+  @observable tracks: TrackProps[] = [{
     name: 'kickdrum',
     type: TrackType.drum,
     sound: [1],
     divider: 2,
-    playType: PlayTypes.oddQuarter
+    playType: PlayTypes.oddQuarter,
   }];
 
   @computed public get drums() {
-    return this.tracks.filter(track => track.type === TrackType.drum);
+    return this.tracks.filter((track) => track.type === TrackType.drum);
   }
 
   @action _remove(track: TrackProps) {
   }
+
   @action _add(track: TrackProps) {
   }
 
   // bind this
   @action add = this._add.bind(this);
+
   @action remove = this._remove.bind(this);
 }
