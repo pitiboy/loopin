71a466fffb40def5594d7f9f5a0bd69f0e0b573b
pitiboy <peter.horvath@finnoconsult.at>
2019-12-29 17:29:55 +0100

    Updates

---
+++ precache-manifest.983cd405e98b7028e95019b9a894e61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621bcb5f5a2c6fb7639974d6bd8e031a",
+    "url": "/loopin/index.html"
+  },
+  {
+    "revision": "13c4bec3dcc260e4efe3",
+    "url": "/loopin/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "9dbd2abf7912cb7e4d82",
+    "url": "/loopin/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "13c4bec3dcc260e4efe3",
+    "url": "/loopin/static/js/2.517b2206.chunk.js"
+  },
+  {
+    "revision": "9dbd2abf7912cb7e4d82",
+    "url": "/loopin/static/js/main.57f7b604.chunk.js"
+  },
+  {
+    "revision": "fdba256ffa6549e66c96",
+    "url": "/loopin/static/js/runtime-main.360b2fa6.js"
+  }
+]);
